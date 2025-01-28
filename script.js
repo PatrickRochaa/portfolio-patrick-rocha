a67c580a277494c7e749d84851ac4340a21a78fe
Patrick Rocha <patrick.rochapereira@hotmail.com>
2025-01-28 18:08:50 -0300

    novo layout

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
+const menuOpener = document.querySelector(".menu-opener");
+const nav = document.querySelector("header nav");
 
-let count = 1;
-
-document.getElementById("radio1").checked = true;
-
-setInterval( function(){
-    nextImage();
-}, 3000)
-
-function nextImage(){
-    count++;
-    if(count>6){
-        count = 1;
-    }
-    document.getElementById("radio"+count).checked = true;
-}
-
-function mudouTamanho(){
-    if( window.innerWidth >=768){
-       itens.style.display = 'block'
-    } else {
-        itens.style.display = 'none'
-    }
-}
-
-function clickMenu(){
-    if (itens.style.display == 'block'){
-    itens.style.display = 'none'
-    } else {
-        itens.style.display = 'block'
-    }
-}
-
+menuOpener.addEventListener("click", () => {
+  nav.classList.toggle("opened");
+  menuOpener.classList.toggle("opened");
+});
