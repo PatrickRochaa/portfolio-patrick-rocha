7e9f6a08613518e5c4289baaf8c57e0b1c4a07b2
Patrick Rocha <patrick.rochapereira@hotmail.com>
2025-01-28 18:21:52 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ menuOpener.addEventListener("click", () => {
   nav.classList.toggle("opened");
   menuOpener.classList.toggle("opened");
 });
+
+document.addEventListener("click", function (event) {
+  // Seleciona todos os overlays
+  const overlays = document.querySelectorAll(".overlay");
+
+  overlays.forEach((overlay) => {
+    // Verifica se o clique foi dentro do overlay ou do botão
+    const projeto = overlay.closest(".projeto");
+    if (!projeto.contains(event.target)) {
+      overlay.style.opacity = "0"; // Esconde o overlay
+    }
+  });
+});
+
+/* =================================================== */
+
+// Mostrar o overlay quando o projeto for clicado
+const projetos = document.querySelectorAll(".projeto");
+projetos.forEach((projeto) => {
+  projeto.addEventListener("click", function () {
+    const overlay = projeto.querySelector(".overlay");
+    overlay.style.opacity = "1"; // Mostra o overlay
+  });
+});
