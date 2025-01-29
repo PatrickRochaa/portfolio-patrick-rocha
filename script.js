912e63e0c97ef0159eb3bf8730c8ab0ad4ea4792
Patrick Rocha <patrick.rochapereira@hotmail.com>
2025-01-29 19:37:13 -0300

    atualizando
    ajustando detalhes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function showSection(sectionId) {
   // Seleciona todas as seções da página
   const sections = document.querySelectorAll("section");
-  
+
   // Esconde todas as seções e define a opacidade como 0 para ocultá-las
   sections.forEach((section) => {
     section.style.display = "none";
@@ -16,7 +16,7 @@ function showSection(sectionId) {
 
   // Obtém a seção correspondente ao ID passado como argumento
   const sectionToShow = document.getElementById(sectionId);
-  
+
   // Torna a seção visível alterando o display para "block"
   sectionToShow.style.display = "block";
 
@@ -27,16 +27,13 @@ function showSection(sectionId) {
 
   // Atualiza os links do menu para destacar o ativo
   const navLinks = document.querySelectorAll("nav a");
-  
+
   // Remove a classe "active" de todos os links do menu
-  navLinks.forEach((link) => {
-    link.classList.remove("active");
-  });
+  navLinks.forEach((link) => { link.classList.remove("active"); });
 
   // Seleciona o link correspondente à seção exibida
   const activeLink = document.querySelector(`nav a[href="#${sectionId}"]`);
 
-   
   // Se o link correspondente existir, adiciona a classe "active" para destacá-lo
   if (activeLink) {
     activeLink.classList.add("active");
