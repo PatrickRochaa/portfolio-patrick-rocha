25082429b3d5f9add4c715d3b2e12eeec07b81a8
Patrick Rocha <patrick.rochapereira@hotmail.com>
2025-01-30 12:11:01 -0300

    detalhes

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 // Aguarda o carregamento do DOM antes de executar a função
 document.addEventListener("DOMContentLoaded", () => {
+  // Adiciona a classe 'show' no header para animá-lo
+  document.querySelector("header").classList.add("show");
+
+  // Adiciona a classe 'show' na seção "sobre"
+  const sobreSection = document.querySelector("section#sobre");
+  if (sobreSection) {
+    sobreSection.classList.add("show");
+  }
+
   showSection("sobre"); // Exibe a seção "sobre" por padrão ao carregar a página
 });
 
+// Função para exibir a seção específica
 function showSection(sectionId) {
-  // Seleciona todas as seções da página
-  const sections = document.querySelectorAll("section");
+  hideAllSections(); // Esconde todas as seções
+  showSectionById(sectionId); // Exibe a seção específica
+  updateNavLinks(sectionId); // Atualiza os links do menu para destacar o ativo
+}
 
-  // Esconde todas as seções e define a opacidade como 0 para ocultá-las
+// Função para esconder todas as seções
+function hideAllSections() {
+  const sections = document.querySelectorAll("section");
   sections.forEach((section) => {
     section.style.display = "none";
     section.style.opacity = 0;
   });
+}
 
-  // Obtém a seção correspondente ao ID passado como argumento
+// Função para exibir uma seção específica com animação de transição
+function showSectionById(sectionId) {
   const sectionToShow = document.querySelector(`section#${sectionId}`);
-
-  // Torna a seção visível alterando o display para "block"
   if (sectionToShow) {
     sectionToShow.style.display = "block";
-
     // Pequeno atraso para garantir que a transição de opacidade ocorra corretamente
     setTimeout(() => {
       sectionToShow.style.opacity = 1;
     }, 10);
   }
+}
 
-  // Atualiza os links do menu para destacar o ativo
+// Função para atualizar os links do menu, destacando o link ativo
+function updateNavLinks(sectionId) {
   const navLinks = document.querySelectorAll("nav a");
 
   // Remove a classe "active" de todos os links do menu
@@ -34,10 +49,8 @@ function showSection(sectionId) {
     link.classList.remove("active");
   });
 
-  // Seleciona o link correspondente à seção exibida
+  // Adiciona a classe "active" ao link correspondente à seção exibida
   const activeLink = document.querySelector(`nav a[href="#${sectionId}"]`);
-
-  // Se o link correspondente existir, adiciona a classe "active" para destacá-lo
   if (activeLink) {
     activeLink.classList.add("active");
   }
